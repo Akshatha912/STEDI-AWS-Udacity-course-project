b6709ac3236e20fabb12c3d4457454190edafae4
Akshatha912 <127100110+Akshatha912@users.noreply.github.com>
2025-11-13 20:01:55 +0530

    Update customer_landing_to_trusted.py

--- customer_landing_to_trusted.py
+++ customer_landing_to_trusted.py
@@ -1,10 +1,9 @@
 import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
 from awsglue.context import GlueContext
+from pyspark.context import SparkContext
 from awsglue.job import Job
-import re
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -13,31 +12,24 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node S3 bucket
-S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
+# Read landing zone data
+customer_landing = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="customer_landing"
+)
+
+# Keep only customers who shared data for research
+customer_trusted = Filter.apply(
+    frame=customer_landing,
+    f=lambda x: x["shareWithResearchAsOfDate"] is not None
+)
+
+# Write filtered data to trusted zone
+glueContext.write_dynamic_frame.from_options(
+    frame=customer_trusted,
     connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://project-stedi-customer-landing-bucket/customers/landing/"],
-        "recurse": True,
-    },
-    transformation_ctx="S3bucket_node1",
-)
-
-# Script generated for node ApplyMapping
-ApplyMapping_node2 = Filter.apply(
-    frame=S3bucket_node1,
-    f=lambda row: (not (row["shareWithResearchAsOfDate"] == 0)),
-    transformation_ctx="ApplyMapping_node2",
-)
-
-# Script generated for node AWS Glue Data Catalog
-AWSGlueDataCatalog_node1676574309040 = glueContext.write_dynamic_frame.from_catalog(
-    frame=ApplyMapping_node2,
-    database="stedi_database",
-    table_name="customer_trusted",
-    transformation_ctx="AWSGlueDataCatalog_node1676574309040",
+    connection_options={"path": "s3://project-stedi-customer-landing-bucket/customer/trusted/"},
+    format="json"
 )
 
 job.commit()
